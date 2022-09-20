4bca7e899a2f5f0f859af6f79c41b6ea336f808c
luka-didham <lukadidham@gmail.com>
2022-09-20 19:07:01 +1200

    Updating chromosomes

--- cosc343_snakes/my_agent.py
+++ cosc343_snakes/my_agent.py
@@ -23,9 +23,7 @@ class Snake:
         self.actions = actions
         self.chromosome = [] #list of chromosomes holding [x,x,x] values representing each action
         for c in range(self.nPercepts):
-            self.chromosome[c] = np.random.uniform(-1, 1, 3)
-
-
+            self.chromosome.append(np.random.uniform(-1, 1, 3)) #inner variable chromosome values for each action (left, straight, right)
 
 
 
@@ -44,8 +42,6 @@ class Snake:
         # 1 - move forward
         # 2 - move right
         actions = [0.0,0.0,0.0]
-
-        chomosome_split = np.array_split(self.chromosome, 3)
         percepts_flatten = percepts.flatten()
         #
         # Different 'percepts' values should lead to different 'actions'.  This way the agent
@@ -53,13 +49,13 @@ class Snake:
         #
         # Different 'self.chromosome' should lead to different 'actions'.  This way different
         # # agents can exhibit different behaviour.
-        count = 0
-        while(count<3):
-            for c in chomosome_split[count]:
-                for x in percepts_flatten:
-                    actions[count] = actions[count] + c*x
-            actions[count] = actions[count] #+ random.uniform(0,1*perceptFieldOfVision) #random bias
-            count = count +1
+
+        for c in self.chromosome:  # each individual nested array
+            for x in percepts_flatten:  # each percept value -1,1,0,2
+                for a in c:  # each individual chromosome value for each nested array relating to each action
+                    print(x)
+                    print(np.where(a))
+                    actions[np.where(a)] = actions[np.where(a)] + a * x
 
         return self.actions[np.argmax(actions)]
 
